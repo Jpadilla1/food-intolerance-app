7ed561de7d16178a619711ce34528cf15f76e35c
Jose Padilla <jose@hausanalytics.com>
2024-09-02 19:41:25 -0700

    spacing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import { Viewport } from "next";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center pt-8 pl-4 pr-4 pb-12">
+    <main className="flex min-h-screen flex-col items-center pt-8 pl-4 pr-4 pb-8">
       <h1 className="text-3xl font-bold text-center mb-4">Food Intolerances</h1>
       <div className="w-full border-b border-divider mb-4" />
       <IntoleranceList
